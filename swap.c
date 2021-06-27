3a5271e71aa7d8d5824a4463ab151cc3f72a7558
Raja-10 <72990829+Raja-10@users.noreply.github.com>
2021-06-27 11:20:11 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -3,11 +3,11 @@
 
 void swap_max(int arr[], int l, int n){
 int  maxval=1000;
-int index=0;
+int index_=0;
   for(int i=n;i<l;i++){
-      if (maxval<arr[i])
+      if(maxval<arr[i])
          maxval=arr[i]; 
-         index=i;
+         index_=i;
     }
     arr[index]=arr[n];
     arr[n]=maxval;
